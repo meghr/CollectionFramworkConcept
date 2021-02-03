9a3cfbe89bbce84a0bf9f29bfdeed85267690a1d
Meghraj <42311362+meghr@users.noreply.github.com>
2021-02-03 11:29:11 +0530

    Hash Map Comparison

--- src/com/company/HashMapDemo.java
+++ src/com/company/HashMapDemo.java
@@ -1,8 +1,6 @@
 package com.company;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class HashMapDemo {
 
@@ -55,6 +53,89 @@ public class HashMapDemo {
         System.out.println("*************iterator using Java 8 For each ********************");
 
         capitalMap.forEach((k,v) -> System.out.println("Key : "+ k + "value : " + v));
+
+    }
+
+    public static void HashMapCompare() {
+
+        HashMap<Integer ,String> map1 = new HashMap<Integer ,String>();
+
+        map1.put(1,"A");
+        map1.put(2,"B");
+        map1.put(3,"C");
+
+
+        HashMap<Integer ,String> map2 = new HashMap<Integer ,String>();
+
+        map2.put(3,"C");
+        map2.put(1,"A");
+        map2.put(2,"B");
+
+
+        HashMap<Integer ,String> map3 = new HashMap<Integer ,String>();
+
+        map3.put(3,"C");
+        map3.put(1,"A");
+        map3.put(2,"B");
+        map3.put(3,"D");
+
+        // 1. on the basis of key value
+
+        System.out.println(map1.equals(map2)); // true
+        System.out.println(map1.equals(map3)); // false
+
+        // 2. Compare hash map for the same keys
+
+        System.out.println(map1.keySet().equals(map2.keySet())); // true
+        System.out.println(map1.keySet().equals(map3.keySet())); // true as key set are same and set ignore the duplicate value hence both are equal
+
+        // 3. Find out the extra keys
+
+        HashMap<Integer ,String> map4 = new HashMap<Integer ,String>();
+
+        map4.put(3,"C");
+        map4.put(1,"A");
+        map4.put(2,"B");
+        map4.put(4,"D");
+
+        // combine the key from both hash map using Hash set
+
+        HashSet<Integer> combinedKeys = new HashSet<>(map1.keySet());
+
+        // Add the key set from map4 as well
+
+        combinedKeys.addAll(map4.keySet());
+
+        // Now removeAll all keyset from map1
+
+        combinedKeys.removeAll(map1.keySet());
+
+        System.out.println(combinedKeys);
+
+        // 4. Compare Hash maps by values
+
+        HashMap<Integer ,String> map5 = new HashMap<Integer ,String>();
+
+        map5.put(1,"A");
+        map5.put(2,"B");
+        map5.put(3,"C");
+        map5.put(4,"B");
+
+
+        HashMap<Integer ,String> map6 = new HashMap<Integer ,String>();
+
+        map6.put(1,"A");
+        map6.put(2,"C");
+        map6.put(3,"C");
+        map6.put(4,"B");
+
+        // Duplicate value are not allowed : using ArrayList
+
+        System.out.println(new ArrayList<>(map5.values()).equals(new ArrayList<>(map6.values()))); // false
+
+        // Duplicate are  allowed : Using HashSet
+
+        System.out.println(new HashSet<>(map5.values()).equals(new HashSet<>(map6.values()))); // true
 
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,6 +23,8 @@ public class Main {
 
         HashMapDemo.HashMapBasics();
 
+        HashMapDemo.HashMapCompare();
+
 
 
 
